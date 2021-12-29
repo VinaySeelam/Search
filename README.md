3f8308f4d415e9049ae10015a3624982d946509c
VinaySeelam <venkatavinaykumarseelam@gmail.com>
2021-12-29 07:52:47 +0530

    Removing hard coding data from step definitions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Search
+
+# ALM Self Service
+Repository requested by [VinaySeelam]( https://github.com/VinaySeelam/Search)
+
+## Useful Git Commands
+| Function                         | Command                            |
+|----------------------------------|------------------------------------|
+| Clone a repo                     | `git clone <repo-url>`             |
+| Identify current status          | `git status`                       |
+| Find out about remote updates    | `git fetch`                        |
+| Pull changes to working copy     | `git pull`                         |
+| Checkout an existing branch      | `git checkout <branch-name>`       |
+| Create and checkout a new branch | `git checkout -b <branch-name>`    |
+| Add changed file to a commit     | `git add <filename>`               |
+| Commit changeset                 | `git commit -m "<commit-message>"` |
+| Push changes                     | `git push`                         |
+* [Git documentation](https://www.google.com/search?q=git+documentation)
+
+## Synopsis
+This project will help to validate different API's from "waarkoop-server.herokuapp.com"
--- Search/src/test/java/stepdefinations/StepDefinations.java
+++ Search/src/test/java/stepdefinations/StepDefinations.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.path.json.JsonPath;
@@ -16,15 +15,23 @@ import static io.restassured.RestAssured.*;
 
 public class StepDefinations {
 	
+	public final String BASE_URI = "https://waarkoop-server.herokuapp.com/";
+	public final int SUCCESS_CODE = 200;
+	public final int ERROR_CODE = 404;
+	public final String ERROR_VALUE = "true";
+	public final String ERROR_MESSAGE = "Not found";
+	public final String RESOURCE = "api/v1/search/test/";
+	
+	
 	RequestSpecification req;
 	ResponseSpecification responsespi;
 	RequestSpecification res;
 	Response response;
-	
+		
 	@Given("Accessing product payload")
 	public void accessing_product_payload() throws IOException {
 		
-		 req = new RequestSpecBuilder().setBaseUri("https://waarkoop-server.herokuapp.com/").build();
+		 req = new RequestSpecBuilder().setBaseUri(BASE_URI).build();
 		 responsespi = new ResponseSpecBuilder().build();
 		 
 		res = given().spec(req).log().all();
@@ -32,35 +39,36 @@ public class StepDefinations {
 
 	@When("he calls {string} product from resource")
 	public void he_calls_product_from_resource(String product) throws IOException {
-		
-		String resource  = "api/v1/search/test/"+product+"";
+				
+		String resource  = RESOURCE+product;
 		
 		if (product.equalsIgnoreCase("car")) {
-			response = res.when().get(resource).then().log().all().spec(responsespi).assertThat().statusCode(404).extract().response();
+			response = res.when().get(resource).then().log().all().spec(responsespi).assertThat().statusCode(ERROR_CODE).extract().response();
 		} else {
-			response = res.when().get(resource).then().log().all().spec(responsespi).assertThat().statusCode(200).extract().response();
+			response = res.when().get(resource).then().log().all().spec(responsespi).assertThat().statusCode(SUCCESS_CODE).extract().response();
 		}
 	
 	}
 
 	@Then("results should be displayed")
 	public void results_should_be_displayed() throws IOException {
-		assertEquals(response.getStatusCode(),200);
+		
+		assertEquals(response.getStatusCode(),SUCCESS_CODE);
 		
 	}
 
 	@Then("he don't see the results")
 	public void he_don_t_see_the_results() throws IOException{
-		
+				
 		String res = response.asString();
 		JsonPath js = new JsonPath(res);
 		
 		String error = js.getString("detail.error");
 		String message = js.getString("detail.message");
 		
-		assertEquals(response.getStatusCode(),404);
-		assertEquals(error,"true");
-		assertEquals(message,"Not found");
+		assertEquals(response.getStatusCode(),ERROR_CODE);
+		assertEquals(error,ERROR_VALUE);
+		assertEquals(message,ERROR_MESSAGE);
 		
 	}
 
